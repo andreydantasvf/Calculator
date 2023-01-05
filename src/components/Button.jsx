962cc8fe59f78db9dc4bc1f498f2d55008fca5c5
Andrey Dantas <andreydantasvf@gmail.com>
2023-01-05 17:59:17 -0300

    Corrigindo escrita da função

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Button.css";
 
-export default (props) => {
+export default function Button(props) {
   return <button className="button">{props.label}</button>;
-};
+}
